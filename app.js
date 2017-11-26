5006d5d92c1798b26d277b17b349dace20529ec7
Nima Rahbari <nima.rahbari@gmail.com>
2017-11-26 16:08:40 +0100

    fixed small issue

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (res, req) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
